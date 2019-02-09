7b26dd33d7203b75efb4f067bf1d5bfda67213fc
skvorekn <skvorekn@miamioh.edu>
2019-02-09 10:19:24 -0500

    meant for CS50 sandbox

---
+++ hello.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+
+    /* get a string from a user and store it in a string variable */
+    string name = get_string("What is your name?\n:")
+
+    printf("hello, name\n");
+}
+
+/* clang -o hello hello.c */
+/* ./hello */
+/* make hello */
+/* gives more configuration options */
+/* make is not a compiler, it is just a program that uses clang in an automated way */
+
+/* error hello.c:line number:character: error:
